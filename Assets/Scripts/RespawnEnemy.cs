ce4e7762797cdfdd773b6414712a09bbf5eb4631
TwinFrame <77979819+TwinFrame@users.noreply.github.com>
2021-04-20 17:54:42 +0300

    AddCheckCollidingHumsterBirth

--- Assets/Scripts/RespawnEnemy.cs
+++ Assets/Scripts/RespawnEnemy.cs
@@ -8,35 +8,48 @@ public class RespawnEnemy : MonoBehaviour
 {
 	[SerializeField] private GameObject _enemy;
 	[SerializeField] private float _durationRespawn;
+	[SerializeField] ContactFilter2D _filter;
 
-	private Transform[] _respawnPointsWithParrent;
-	private Transform[] _respawnPoints;
+	private GameObject _currentEnemy;
+	private RespawnPoint[] _respawnPoints;
 	private int _currentPoint;
 	private Collider2D _wheelFrame;
+	private Collider2D[] _colliders = new Collider2D[1];
+	private Vector3 _offsetInstantiatePosition;
 
 	private void Start()
 	{
 		_wheelFrame = GameObject.FindObjectOfType<WheelFrame>().GetComponent<PolygonCollider2D>();
 
-		_respawnPointsWithParrent = GetComponentsInChildren<Transform>();
-
-		_respawnPoints = new Transform[_respawnPointsWithParrent.Length - 1];
-
-		for (int i = 0; i < _respawnPointsWithParrent.Length - 1; i++)
-		{
-			_respawnPoints[i] = _respawnPointsWithParrent[i + 1];
-		}
+		_respawnPoints = GetComponentsInChildren<RespawnPoint>();
 
 		var creatingEnemiesJob = StartCoroutine(CreatingEnemiesByPoints(_respawnPoints));
 	}
 
-	private IEnumerator CreatingEnemiesByPoints(Transform[] _points)
+	private IEnumerator CreatingEnemiesByPoints(RespawnPoint[] _points)
 	{
 		_currentPoint = 0;
 
 		while (true)
 		{
-			Instantiate(_enemy, _points[_currentPoint].transform.position, Quaternion.identity);
+			Physics2D.OverlapBox(new Vector2(_points[_currentPoint].transform.position.x, _points[_currentPoint].transform.position.y),
+				new Vector2(0.5f, 0.5f), 0, _filter, _colliders);
+
+			if (_colliders[0] == _wheelFrame)
+			{
+				if (_points[_currentPoint].transform.position.y > 0)
+					_offsetInstantiatePosition = new Vector3(0, -1f, 0);
+				else
+					_offsetInstantiatePosition = new Vector3(0, 1f, 0);
+			}
+			else
+			{
+				_offsetInstantiatePosition = new Vector3(0, 0, 0);
+			}
+
+			_colliders = new Collider2D[1];
+
+			_currentEnemy = Instantiate(_enemy, _points[_currentPoint].transform.position + _offsetInstantiatePosition, Quaternion.identity);
 
 			_currentPoint++;
 
